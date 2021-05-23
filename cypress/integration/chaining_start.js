c84253fabc280864b66ebd938ecb0a6b2abea008
Ronald van der Louw <r.van.der.louw@gmail.com>
2021-05-23 15:36:51 +0200

    fix typo

--- cypress/integration/chaining_start.js
+++ cypress/integration/chaining_start.js
@@ -1,15 +1,9 @@
 /// <reference types="cypress" />
 
 beforeEach(() => {
+  cy.visit("/board/2305140181");
+});
 
-  cy
-    .visit('/board/2305140181')
-})
-
-it('Chaining commands', () => {
-
-  cy
-    .get('[data-cy="task]')
-    .should('be.visible')
-
+it("Chaining commands", () => {
+  cy.get("[data-cy=task]").should("be.visible");
 });
